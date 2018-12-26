4dbf7b6a5e2f4b84c9a11586ef8b11e8cf62c6ad
Alex Rossi Ignez <rx.rossi@gmail.com>
2018-12-26 14:36:44 +0000

    Change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const webpack = require("webpack")
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'webserverBuild'),
+    filename: 'bundle.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index_template.html"
+      template: 'index_template.html',
     }),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
   ],
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'webserverBuild'),
     quiet: true,
     hot: true,
-    port: 9000
+    port: 9000,
   },
   module: {
     rules: [
       {
         test: /\.m?(js|jsx)$/,
         exclude: /node_modules/,
-        use: "babel-loader"
+        use: 'babel-loader',
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  }
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 }
