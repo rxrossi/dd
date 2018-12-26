34a37b20c067ef0aeeba40c8928380f5cb64fd95
Alex Rossi Ignez <rx.rossi@gmail.com>
2018-12-26 14:52:00 +0000

    Another build try

--- index.js
+++ index.js
@@ -2,7 +2,32 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow() {
   win = new BrowserWindow({ width: 1200, height: 800 })
-  win.loadURL('http://localhost:9000/')
+  win.loadFile('webserverBuild/index.html')
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
 }
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
 
 app.on('ready', createWindow)
